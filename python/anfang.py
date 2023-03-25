4321f47e9d8dc56e0b9109ace7723ea03957e4e1
Judith <judith.leson@stud.uni-due.de>
2023-03-25 09:30:05 +0100

    29.3., python-Ordner aktualisiert

--- python/anfang.py
+++ python/anfang.py
@@ -124,15 +124,15 @@ for i in range(len(liste[0])):
 
 #testen auf nicht veraenderte Werte:
 def aenderung(x):
-   global liste
+   global liste 
    global ver
-   vorwert = liste[1][x] #1. Datenzeile
+   vorwert = liste[1][x] #1. Datenzeile 
    for a in range(2, len(liste)):
            b = liste[a][x]
            if b != vorwert:
                ver += [a]
                return ""
-   return str(liste[0][x]) + b +"_" #Generiert String, der die identischen Parameter aneinanderreiht
+   return str(liste[0][x]) + b +"_" #Generiert String, der die identischen Parameter aneinanderreiht 
 
 ver = [] # Array der Indizes der veraenderten Spaltenwerte (ausser der Energien)
 gleich = aenderung(0) 
@@ -154,67 +154,16 @@ liste = Durchgehen(liste)
 
 l = sorted(liste[1:], key=lambda a_entry: a_entry[3])#xDist
 l = sorted(l, key=lambda a_entry: a_entry[4])#zDist
-genauigkeit = 6
-
 
 f = open("umsortiert.txt", "w")
 for a in liste[0]:
     f.write(str(a) + "\t" )
 f.write("\n")
-for a in range(len(l)):
-    for b in range(len(l[a])):
-        if len(str(l[a][b])) > 6:
-            try:
-                c = "%0."+str(genauigkeit)+ "f" # muss zweizeilig aufgeteilt sein
-                l[a][b] = c % round(l[a][b], genauigkeit) 
-            except: 
-                pass
-        f.write(str(l[a][b])  + "\t")
+for a in l:
+    for b in a:
+        f.write(str( b)  + "\t")
     f.write("\n"  ) 
 f.close() 
-
-
-
-# Liste = l
-
-def z(l):  # Suchen der Indizes der veränderten Werte in Array mit Unterarrays 
-    a = len(l) 
-    if a == 0: return
-    b = len(l[0])
-    v = [] # Array für geänderte Werte
-    for k in range(b):
-        x = l[0][k]
-        for j in range(1,a):
-            if l[j][k] != x:
-                v += [k] 
-                break 
-    return v
-
-
-def raus_kurz(l,dateiname): 
-    global liste 
-    ver = z(l) 
-    f = open(dateiname+"-kurz.txt", "w")
-    #Schreiben der unveränderten Werte als Überschrift 
-    for k in range(len(l[0])):
-        if k not in ver:
-            f.write( str( liste[0][k]) + " "+ str( l[0][k] ) + ", ") 
-    f.write("\n\n")
-    #Tabellekopf:
-    for k in range(len(liste[0] ) ) : 
-        if k in ver: 
-            f.write( str(liste[0][k] )+ "\t" ) 
-    f.write("\n") 
-    #Werte: 
-    for k in l:
-        for j in range(len(k)) :
-            if j in z(l): 
-                f.write(str(k[j] ) + "\t")
-        f.write("\n")
-    f.close()
-    return
-
-raus_kurz(l, dateiname = "uebersicht")
 
 
 #Liste fuer die Systeme gleicher Entfernung
@@ -239,73 +188,35 @@ for a in range(1,len(l)):
 
 
 
-
-#Diagramm bzw. Übersichtsvergleich: 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Diagramm bzw. Übersichtsvergleich:  
 
 import matplotlib.pyplot as plt
 
 for a in range(len(L)) : #ueber Anzahl variierter techn.Parameter-Saetze 
-    raus_kurz(L[a], "techPar-"+str(a) ) 
-    '''
-
-    try:
-        f = open("asdk.txt", "a")
-    except:
-        f = open("asdk.txt", "w")
-    f.write("\n")
-
-    Titel = "" # Systemparameter
-    n ="" # unveränderte Parameter 
-    kopf = "" 
-    ver =[]
-    for c in range( len( liste[0] ) ) : #L[a][0] ist durch mehr Eigenwerte ggf. länger 
-        #print( L[a][0] ) 
-        if c < 5: #danach kommen keine Systemparameter mehr
-            Titel += str(liste[0][c]) + str( L[a][0][:5][c] )+ "_"
-        if c != 0 and L[a][0][c] == L[a][0][0]:
-            n +=  str(liste[0][c]) + str( L[a][0][c] )+ "_"
-            ver += [c] 
-        else: #veränderte Werte 
-            kopf +=  str(liste[0][c])  + "\t" 
-    #print(Titel,"\n", n) 
-    f.write(Titel + "\t" + n + "\n\n" )
-    f.write(kopf) 
-    f.write("\n")
-
     x = [0]
     for b in range(1,len(L[a]) ):
         x += [b]
     y = [ L[a][0][i] ]
     for b in range(1,len(L[a]) ) :
         y += [ L[a][b][i] ]
-        for c in range(len(L[a][b])):
-            if c in ver or c >= i:
-                f.write( str(  L[a][b][c] ) + "\t") #nicht mehr runden hier, da zuvor in l schon gerunden = zurück in String verändert 
-        f.write("\n") 
     #print(x, y) 
-
-    Diagramm(y = y , x = x, Titel =Titel[:-1]  )
-    plt.savefig("TecVar"+ Titel[:-1] +".pdf") 
+    #Titel = ""
+    n ="" # str(a) 
+    for c in range( len( L[a][b][:5] ) ) :
+        Titel = str(liste[0][c]) + str( L[a][b][:5][c] )+ "_"
+        #if c not in ver:
+        n +=  str(liste[0][c]) + str( L[a][b][:5][c] )+ "_"
+    #print( Titel,"\n", n) 
+    Diagramm(y = y , x = x, Titel =n[:-1]  )
+    plt.savefig("TecVar"+ n[:-1] +".pdf") 
     plt.close() 
 
-    f.write("\n\n\n") 
-    f.close() 
 
-'''
+
+
+
+
+
+
+
 
